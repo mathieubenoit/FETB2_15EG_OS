d6110983d317fde60e67d44e7ee70397734eed11
Mathieu Benoit <mathieu.benoit@cern.ch>
2021-12-29 17:05:52 -0500

    Added script to configure clock chip at startup, added SPI bus for one of the ADC, and SPIDEV drivers for kernel in user space.

--- tarball.sh
+++ tarball.sh
@@ -1,1 +1,2 @@
+rm FETB2OS_latest.tar.gz
 tar cvzfh FETB2OS_latest.tar.gz images/linux/petalinux-sdimage.wic images/linux/Image images/linux/system.bit images/linux/boot.scr images/linux/BOOT.BIN images/linux/rootfs.tar.gz
