941bdaacd6f9f9aeb7949bf85416966893341790
Mathieu Benoit <mathieu.benoit@cern.ch>
2022-03-08 16:08:56 -0500

    new fpga file

--- project-spec/hw-description/psu_init.c
+++ project-spec/hw-description/psu_init.c
@@ -20245,6 +20245,26 @@ unsigned long psu_afi_config(void)
     /*
     * AFIFM INTERFACE WIDTH
     */
+    /*
+    * Register : afi_fs @ 0XFD615000
+
+    * Select the 32/64/128-bit data width selection for the Slave 0 00: 32-bit
+    *  AXI data width (default) 01: 64-bit AXI data width 10: 128-bit AXI data
+    *  width 11: reserved
+    *  PSU_FPD_SLCR_AFI_FS_DW_SS0_SEL                              0x0
+
+    * Select the 32/64/128-bit data width selection for the Slave 1 00: 32-bit
+    *  AXI data width (default) 01: 64-bit AXI data width 10: 128-bit AXI data
+    *  width 11: reserved
+    *  PSU_FPD_SLCR_AFI_FS_DW_SS1_SEL                              0x0
+
+    * afi fs SLCR control register. This register is static and should not be
+    * modified during operation.
+    * (OFFSET, MASK, VALUE)      (0XFD615000, 0x00000F00U ,0x00000000U)
+    */
+	PSU_Mask_Write(FPD_SLCR_AFI_FS_OFFSET, 0x00000F00U, 0x00000000U);
+/*##################################################################### */
+
 
 	return 1;
 }
